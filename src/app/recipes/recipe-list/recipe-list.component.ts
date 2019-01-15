d42c796f585141191f5c9a56df76a0c3f8ea1419
Vladislav Kostevich <v.kostevich@intetics.com>
2019-01-15 11:54:19 +0300

    commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Recipe Name', 'Recipe Description', 'https://cdn-image.myrecipes.com/sites/default/files/styles' +
+      '/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA')
+  ];
 
   constructor() { }
 
